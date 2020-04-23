27ac2b3dc146b34b8fcb4910cc87ca57b7817146
Jon Wright <jwright.taipei@gmail.com>
2020-04-23 08:38:56 +0800

    removed old files, fixes for the web consurf numbering

--- get_consurf_numbers.py
+++ get_consurf_numbers.py
@@ -59,7 +59,6 @@ if len(sys.argv) == 1:
 # Open the file,
 INFILE = open(sys.argv[1],"r")
 OUTFILE = open("r4s_pdb.py","w")
-print (">PDB_ATOM")
 index=0
 r4sindex=0
 OUTFILE.write("R4S_2_PDB={\n")
@@ -79,12 +78,8 @@ for TMLINE in INFILE:
             # Convert from 3 letter code to single letter code and print it out
             if resname in RESIDUETAB:
                 single = str(RESIDUETAB.get(resname))
-                print (single, end = "")
-            else: 
-                print (".", end = "")
             if single !="X":
                 r4sindex+=1
                 outline = "    '" + "{:>d}".format(r4sindex) + "' : '" + "{:>d}".format(index) + "' ,\n"
                 OUTFILE.write(outline)
-print ("") # print a newline to end the sequence output
 OUTFILE.write("}")
