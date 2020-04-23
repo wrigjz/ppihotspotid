cbfe1fbd0544cacb4fc7e87229830dd9cb898f3d
Jon Wright <jwright.taipei@gmail.com>
2020-04-23 19:22:05 +0800

    Post pylint

--- sasa_to_perc.py
+++ sasa_to_perc.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
-#########################################################################################################################
+###################################################################################################
 ## and Jon Wright, IBMS, Academia Sinica, Taipei, 11529, Taiwan
 ## These files are licensed under the GLP ver 3, essentially you have the right
 ## to copy, modify and distribute this script but all modifications must be offered
 ## back to the original authors
-#########################################################################################################################
+###################################################################################################
 #
 # This program reads a freesasa output file and then gives a percentage instead
 # The standard values were taken from freesasa
 
 import sys
-import os
 
 # This is the total SASA in the G-X-G tripeptide, we calc'd this ourselves
-SASAHAS={
+SASAHAS = {
     'ASP' : int(160.32),
     'GLY' : int(87.75),
     'ACE' : int(121.09),
@@ -48,23 +47,16 @@ if len(sys.argv) == 1:
     sys.exit(0)
 
 # Open the sasa file,
-SASAFILE = open(sys.argv[1],"r")
-
-# simple way to assign result as an array sized 10000 with initial value -1
-result = [-1] * 10000
+SASAFILE = open(sys.argv[1], "r")
 i = int(0)
 
 for SASALINE in SASAFILE:
     if SASALINE[0:3] != "SEQ":
         continue
-    SASALINE     = SASALINE.rstrip()     # Perl Chomp
-    SASASPLIT    = SASALINE.split(":")   # separate at the :
-    residue      = SASALINE[12:15]
-    temp         = SASASPLIT[1].replace(" ","")
-    sasadom      = (SASAHAS.get(residue))
-    relsasa      = (float(temp) / sasadom)*100       # calculate the relative sasa
-    print(SASALINE[0:10],SASALINE[11:24]," : {:7.2f}".format(relsasa))
-    
-    
-
-
+    SASALINE = SASALINE.rstrip()     # Perl Chomp
+    SASASPLIT = SASALINE.split(":")   # separate at the :
+    residue = SASALINE[12:15]
+    temp = SASASPLIT[1].replace(" ", "")
+    sasadom = (SASAHAS.get(residue))
+    relsasa = (float(temp) / sasadom)*100       # calculate the relative sasa
+    print(SASALINE[0:10], SASALINE[11:24], " : {:7.2f}".format(relsasa))
