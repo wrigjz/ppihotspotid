b26e158918a6a321d37a11aa142af3bde61db055
Jon Wright <jwright.taipei@gmail.com>
2020-04-23 21:04:39 +0800

    Post pylint

--- renumber.py
+++ renumber.py
@@ -1,26 +1,25 @@
 #!bin/python
-#########################################################################################################################
+###################################################################################################
 ## and Jon Wright, IBMS, Academia Sinica, Taipei, 11529, Taiwan
 ## These files are licensed under the GLP ver 3, essentially you have the right
 ## to copy, modify and distribute this script but all modifications must be offered
 ## back to the original authors
-#########################################################################################################################
+###################################################################################################
 import sys
 
 # Quick and dirty script to renumber Backy's output to the pdb2amb output
 
-renumfile = open(sys.argv[1],"r")
-critifile = open(sys.argv[2],"r")
+RENUMFILE = open(sys.argv[1], "r")
+CRITIFILE = open(sys.argv[2], "r")
 
+SAVED = [0 for i in range(0, 2000)]
 
-saved = [0 for i in range(0,1000)]
-
-for LINE in renumfile:
+for LINE in RENUMFILE:
     junk1, old, junk2, new = [x.strip() for x in LINE.split()]
-    index        = int(old)
-    saved[index] = int(new)
+    index = int(old)
+    SAVED[index] = int(new)
 
-for LINE in critifile:
+for LINE in CRITIFILE:
     junk1, junk2, junk3, old = [x.strip() for x in LINE.split()]
-    index=int(old)
-    print(junk1, junk2, junk3, old, saved[index])
+    index = int(old)
+    print(junk1, junk2, junk3, old, SAVED[index])
