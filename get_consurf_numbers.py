4f6597da94cbb1c8b97f7e5b88c0a58608a99a88
Jon Wright <jwright.taipei@gmail.com>
2020-04-23 21:09:47 +0800

    Post pylint

--- get_consurf_numbers.py
+++ get_consurf_numbers.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python
-#########################################################################################################################
+###################################################################################################
 ## and Jon Wright, IBMS, Academia Sinica, Taipei, 11529, Taiwan
 ## These files are licensed under the GLP ver 3, essentially you have the right
 ## to copy, modify and distribute this script but all modifications must be offered
 ## back to the original authors
-#########################################################################################################################
+###################################################################################################
 #This take a PDB file and then creates a file with a fasta format for a single chain
 # It will ouput ACE/NME as X
 # and write another file that allows for the final consurf numbering
 # to be related back to the initial PDB file
 
 import sys
-import os
 
 # Set initial values for a few variables
-previous_resno       = "-999"
-previous_resno_short = "-999"
+PREVIOUS_RESNO = "-999"
 
 # Look up table for converting 3 letter AA codes to single letter codes
 RESIDUETAB = {
@@ -39,7 +37,6 @@ RESIDUETAB = {
     'VAL' : 'V',
     'TYR' : 'Y',
     'TRP' : 'W',
-    'CYS' : 'C',
     'CYX' : 'C',
     'HSP' : 'H',
     'HSD' : 'H',
@@ -57,29 +54,30 @@ if len(sys.argv) == 1:
     sys.exit(0)
 
 # Open the file,
-INFILE = open(sys.argv[1],"r")
-OUTFILE = open("r4s_pdb.py","w")
-index=0
-r4sindex=0
+INFILE = open(sys.argv[1], "r")
+OUTFILE = open("r4s_pdb.py", "w")
+INDEX = 0
+R4SINDEX = 0
 OUTFILE.write("R4S_2_PDB={\n")
 # Loop over each line of the input file splitting it into different columns
 for TMLINE in INFILE:
     if TMLINE[0:4] == "ATOM" or TMLINE[0:6] == "HETATM":
     # Save the info we need for later
-        resname     = TMLINE[17:20]
-        atom_name   = TMLINE[12:16]
-        chain       = TMLINE[21:22]
-        resid_long  = str(TMLINE[23:27])
+        resname = TMLINE[17:20]
+        atom_name = TMLINE[12:16]
+        chain = TMLINE[21:22]
+        resid_long = str(TMLINE[23:27])
         # This block looks for a change in the residue number  , and prints out the 1 letter codes
-        if previous_resno != resid_long:
-            index += 1
+        if PREVIOUS_RESNO != resid_long:
+            INDEX += 1
             # Make a note of the residue number for the next line we look at
-            previous_resno        = resid_long
+            PREVIOUS_RESNO = resid_long
             # Convert from 3 letter code to single letter code and print it out
             if resname in RESIDUETAB:
                 single = str(RESIDUETAB.get(resname))
-            if single !="X":
-                r4sindex+=1
-                outline = "    '" + "{:>d}".format(r4sindex) + "' : '" + "{:>d}".format(index) + "' ,\n"
+            if single != "X":
+                R4SINDEX += 1
+                outline = "    '" + "{:>d}".format(R4SINDEX) + "' : '" + \
+                    "{:>d}".format(INDEX) + "' ,\n"
                 OUTFILE.write(outline)
 OUTFILE.write("}")
