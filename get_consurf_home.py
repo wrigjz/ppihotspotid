be8695dc673fd1863a2791ceb5a0b0f7ed60a802
Jon Wright <jwright.taipei@gmail.com>
2020-04-23 19:14:16 +0800

    Post pylint

--- get_consurf_home.py
+++ get_consurf_home.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-#########################################################################################################################
+###################################################################################################
 ## and Jon Wright, IBMS, Academia Sinica, Taipei, 11529, Taiwan
 ## These files are licensed under the GLP ver 3, essentially you have the right
 ## to copy, modify and distribute this script but all modifications must be offered
 ## back to the original authors
-#########################################################################################################################
+###################################################################################################
 import sys
 
 # Simple script to print out the consurf grades
 #  # SEQ 3LETT PDB COLOUR  SCORE
 #  1  H  HIS    2    6   -0.162
 
-INFILE  = open(sys.argv[1],"r")
-OUTFILE = open(sys.argv[2],"w")
+INFILE = open(sys.argv[1], "r")
+OUTFILE = open(sys.argv[2], "w")
 for LINE in INFILE:
     in1, in2, in3, pdbnum, grade, in6 = [x.strip() for x in LINE.split()]
     if in1 != "#":
--- get_consurf_web.py
+++ get_consurf_web.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-#########################################################################################################################
+###################################################################################################
 ## and Jon Wright, IBMS, Academia Sinica, Taipei, 11529, Taiwan
 ## These files are licensed under the GLP ver 3, essentially you have the right
 ## to copy, modify and distribute this script but all modifications must be offered
 ## back to the original authors
-#########################################################################################################################
+###################################################################################################
 import sys
-import os
+# Open the hash for sequence to wild-mini.pdb renumbering scheme
+from r4s_pdb import R4S_2_PDB
 
 # Simple script to print out the consurf grades, taken from the web site and then renumbered
 # to match the critires internal numbering scheme
-#   1       N        ASN2:X      -0.329            6     -0.744,-0.136                       8,5                   37/150        D,N,L
+#   1       N        ASN2:X      -0.329            6     -0.744,-0.136                       8,5
+#     37/150        D,N,L
 
 # Get the output name from the command line
 if len(sys.argv) <= 2:
@@ -18,16 +20,15 @@ if len(sys.argv) <= 2:
     sys.exit(0)
 
 # Open input and output files
-INFILE  = open(sys.argv[1],"r")
-OUTFILE = open(sys.argv[2],"w")
+INFILE = open(sys.argv[1], "r")
+OUTFILE = open(sys.argv[2], "w")
 
-# Open the hash for sequence to wild-mini.pdb renumbering scheme
-from r4s_pdb import R4S_2_PDB
-seqnum = 0
+SEQNUM = 0
 for TMLINE in INFILE:
     if TMLINE[18:19] == ":":
-        seqnum += 1  # increment the consurf sequence number
-        seqnum_str = str(seqnum) # Done this way to be compatitble with Jon's own CS server
-        original = (R4S_2_PDB.get(seqnum_str)) # get the 'original' resnumber from the wild_mini pdb file for CS
-        score     = TMLINE[31:32]
+        SEQNUM += 1  # increment the consurf sequence number
+        seqnum_str = str(SEQNUM) # Done this way to be compatitble with Jon's own CS server
+        original = (R4S_2_PDB.get(seqnum_str)) # get the 'original' resnumber from
+                                               # the wild_mini pdb file for CS
+        score = TMLINE[31:32]
         OUTFILE.write(" {:>4}".format(original) + " {:>3}".format(score) + "\n")
