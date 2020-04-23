feb061d397f33f927d0399e666ab823df081b7de
Jon Wright <jwright.taipei@gmail.com>
2020-04-23 19:18:19 +0800

    Post pylint, changed == to is for comparing to None

--- print_results.py
+++ print_results.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
-#########################################################################################################################
+###################################################################################################
 ## and Jon Wright, IBMS, Academia Sinica, Taipei, 11529, Taiwan
 ## These files are licensed under the GLP ver 3, essentially you have the right
 ## to copy, modify and distribute this script but all modifications must be offered
 ## back to the original authors
-#########################################################################################################################
-import sys
+###################################################################################################
 
 # This script take the stable/unstable results and then maps them back to the original PDB numbers
 
@@ -14,7 +13,7 @@ from numbers import NUMBERS
 
 CHAINFILE = open("original_chain.txt")
 for CHAINLINE in CHAINFILE:
-        chain = CHAINLINE.strip()
+    chain = CHAINLINE.strip()
 CHAINFILE.close()
 
 #GLU,  84, Unstable
@@ -22,6 +21,8 @@ INFILE = open("results_ambnum.txt")
 for LINE in INFILE:
     resname, resnumber, crititype = [x.strip() for x in LINE.split(",")]
     original = (NUMBERS.get(resnumber))
-    if original == None: original = "NaN"
-    print("{:3s}".format(resname), "{:>4}".format(original), "{:1s}".format(chain), "{:8s}".format(crititype))
+    if original is None:
+        original = "NaN"
+    print("{:3s}".format(resname), "{:>4}".format(original), "{:1s}".format(chain), \
+          "{:8s}".format(crititype))
 INFILE.close()
