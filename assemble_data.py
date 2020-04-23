d94bdba4807db9c4335022d4241365adca5fd0ac
Jon Wright <jwright.taipei@gmail.com>
2020-04-23 20:50:23 +0800

    Post pylint

--- assemble_data.py
+++ assemble_data.py
@@ -1,129 +1,137 @@
 #!/usr/bin/python3
-#########################################################################################################################
+###################################################################################################
 ## and Jon Wright, IBMS, Academia Sinica, Taipei, 11529, Taiwan
 ## These files are licensed under the GLP ver 3, essentially you have the right
 ## to copy, modify and distribute this script but all modifications must be offered
 ## back to the original authors
-#########################################################################################################################
+###################################################################################################
 # Simple script to try to assemble data for the critires project
 # In Backys method for the Energy values she takes the number of residues and divides by 10
-# she then assigns the lowest 10% rank 1, .. next lowest 10% rank 2 .. to rank 9 
+# she then assigns the lowest 10% RANK 1, .. next lowest 10% RANK 2 .. to RANK 9
 # then all the rest get assigned as value 10
-# We set the Energy rank of ACE/NME and their enighbours to 5 and their consuef values to 0
+# We set the Energy RANK of ACE/NME and their enighbours to 5 and their consuef values to 0
 # to make sure these are not selected later on
 
-# Here we try to block the energies into 9 equal sized energy ranges and assign residues to 
+# Here we try to block the energies into 9 equal sized energy ranges and assign residues to
 # each range so it is more like a bell shaped curve
 
-import sys
 # Get a line count for the arrays we need
-num_lines = 1
-num_lines += sum(1 for lines in open("wild_relsasa.txt","r"))
+NUM_LINES = 1
+NUM_LINES += sum(1 for lines in open("wild_relsasa.txt", "r"))
 
 # And then setup the arrays
-resname   = [0 for i in range(0,num_lines)]
-resnumber = [0 for i in range(0,num_lines)]
-relsasa   = [0 for i in range(0,num_lines)]
-int_stab  = [0 for i in range(0,num_lines)]
-vdw_stab  = [0 for i in range(0,num_lines)]
-ele_stab  = [0 for i in range(0,num_lines)]
-pol_stab  = [0 for i in range(0,num_lines)]
-npl_stab  = [0 for i in range(0,num_lines)]
-consurf   = [0 for i in range(0,num_lines)]
-gas_ene   = [0 for i in range(0,num_lines)]
-rank      = [0 for i in range(0,num_lines)]
-grade    = [1 for i in range(0,num_lines)]
-position  = [0 for i in range(0,num_lines)]
-max_resnum = 0
-min_resnum = num_lines
+RESNAME = [0 for i in range(0, NUM_LINES)]
+RESNUMBER = [0 for i in range(0, NUM_LINES)]
+RELSASA = [0 for i in range(0, NUM_LINES)]
+INT_STAB = [0 for i in range(0, NUM_LINES)]
+VMD_STAB = [0 for i in range(0, NUM_LINES)]
+ELE_STAB = [0 for i in range(0, NUM_LINES)]
+POL_STAB = [0 for i in range(0, NUM_LINES)]
+NPL_STAB = [0 for i in range(0, NUM_LINES)]
+CONSURF = [0 for i in range(0, NUM_LINES)]
+GAS_ENE = [0 for i in range(0, NUM_LINES)]
+RANK = [0 for i in range(0, NUM_LINES)]
+GRADE = [1 for i in range(0, NUM_LINES)]
+POSITION = [0 for i in range(0, NUM_LINES)]
+MAX_RESNUM = 0
+MIN_RESNUM = NUM_LINES
 
 # Get the number of residues from the sasa file and the relative sasa value
-INFILE = open("wild_relsasa.txt","r")
+INFILE = open("wild_relsasa.txt", "r")
 for LINE in INFILE:
-    in1, in2, in3, *junk  = [x.strip() for x in LINE.split(",")]
-    resnum             = int(in1)
-    resnumber[resnum]  = int(in1)
-    resname[resnum]    = in2
-    relsasa[resnum]    = float(in3)
+    in1, in2, in3, *junk = [x.strip() for x in LINE.split(",")]
+    resnum = int(in1)
+    RESNUMBER[resnum] = int(in1)
+    RESNAME[resnum] = in2
+    RELSASA[resnum] = float(in3)
 INFILE.close()
 
-# Now we allocate the consurf values
-INFILE = open("wild_consurf.txt","r")
+# Now we allocate the CONSURF values
+INFILE = open("wild_consurf.txt", "r")
 for LINE in INFILE:
     in1, in2 = [x.strip() for x in LINE.split()]
-    resnum          = int(in1)
-    consurf[resnum] = int(in2)
+    resnum = int(in1)
+    CONSURF[resnum] = int(in2)
 INFILE.close()
 
 
 # Now we allocate the energy values
-INFILE = open("stability.txt","r")
+INFILE = open("stability.txt", "r")
 for LINE in INFILE:
     j1, in0, in1, in2, in3, in4, in5, in6 = [x.strip() for x in LINE.split()]
-    resnum           = int(in0)
-    int_stab[resnum] = float(in1)
-    vdw_stab[resnum] = float(in2)
-    ele_stab[resnum] = float(in3)
-    pol_stab[resnum] = float(in4)
-    npl_stab[resnum] = float(in5)
-    position[resnum] = resnum
-    if max_resnum < resnum: max_resnum = resnum
-    if min_resnum > resnum: min_resnum = resnum
+    resnum = int(in0)
+    INT_STAB[resnum] = float(in1)
+    VMD_STAB[resnum] = float(in2)
+    ELE_STAB[resnum] = float(in3)
+    POL_STAB[resnum] = float(in4)
+    NPL_STAB[resnum] = float(in5)
+    POSITION[resnum] = resnum
+    if MAX_RESNUM < resnum:
+        MAX_RESNUM = resnum
+    if MIN_RESNUM > resnum:
+        MIN_RESNUM = resnum
 INFILE.close()
 
 #At this point we need to go through the data and look for the upper and lower gas phase energies
-for j in range(min_resnum,max_resnum+1):
-    gas_ene[j] = int_stab[j] + vdw_stab[j] + ele_stab[j]
-    if j == min_resnum: upper = gas_ene[j]
-    if j == min_resnum: lower = gas_ene[j]
-    if gas_ene[j] > upper: upper = gas_ene[j]
-    if gas_ene[j] < lower: lower = gas_ene[j]
-ene_range   = upper - lower
-bin_width = ene_range / 10
-#print(upper,lower,ene_range, bin_width)
+for j in range(MIN_RESNUM, MAX_RESNUM+1):
+    GAS_ENE[j] = INT_STAB[j] + VMD_STAB[j] + ELE_STAB[j]
+    if j == MIN_RESNUM:
+        upper = GAS_ENE[j]
+    if j == MIN_RESNUM:
+        lower = GAS_ENE[j]
+    if GAS_ENE[j] > upper:
+        upper = GAS_ENE[j]
+    if GAS_ENE[j] < lower:
+        lower = GAS_ENE[j]
+ENE_RANGE = upper - lower
+BIN_WIDTH = ENE_RANGE / 10
+#print(upper,lower,ENE_RANGE, BIN_WIDTH)
 
-# Here we sort them into 9 ranks of equal energy widths in case we use this method
-for j in range(min_resnum,max_resnum+1):
-    rank[j] = 10
-    for k in range(0,10):
-        if gas_ene[j] > (lower + (bin_width * k)):
-            rank[j] = 10 - k
+# Here we sort them into 9 RANKs of equal energy widths in case we use this method
+for j in range(MIN_RESNUM, MAX_RESNUM+1):
+    RANK[j] = 10
+    for k in range(0, 10):
+        if GAS_ENE[j] > (lower + (BIN_WIDTH * k)):
+            RANK[j] = 10 - k
 
-# Here try to sort the residues based on gas_energies, posiiton should now run from lowest E to highest E
+# Here try to sort the residues based on GAS_ENErgies, posiiton should now run from
+# lowest E to highest E
 #for j in range(0,i):
-unsorted = 1
-while unsorted == 1:
-    unsorted = 0
-    for k in range(0,max_resnum):
-        if gas_ene[position[k]] > gas_ene[position[k+1]]:
-            temp_pos       = position[k]
-            position[k]    = position[k+1]
-            position[k+1]  = temp_pos
-            unsorted       = 1
-    if unsorted == 0:
+UNSORTED = 1
+while UNSORTED == 1:
+    UNSORTED = 0
+    for k in range(0, MAX_RESNUM):
+        if GAS_ENE[POSITION[k]] > GAS_ENE[POSITION[k+1]]:
+            temp_pos = POSITION[k]
+            POSITION[k] = POSITION[k+1]
+            POSITION[k+1] = temp_pos
+            UNSORTED = 1
+    if UNSORTED == 0:
         break
 
-# Now we need to grade according to the energies, equal # of residues in each band
-band_width = (max_resnum-min_resnum)/10  # We are sorting into 10 groups
-for k in range(0,10):
-    lower_loop=int(band_width * k)
-    upper_loop=int(band_width * (k+1)) + 1
+# Now we need to GRADE according to the energies, equal # of residues in each band
+BAND_WIDTH = (MAX_RESNUM-MIN_RESNUM)/10  # We are sorting into 10 groups
+for k in range(0, 10):
+    lower_loop = int(BAND_WIDTH * k)
+    upper_loop = int(BAND_WIDTH * (k+1)) + 1
     #print("lower",lower_loop, upper_loop,k)
-    for j in range(lower_loop,upper_loop):
-        grade[position[j]] = 10 - k
-        
+    for j in range(lower_loop, upper_loop):
+        GRADE[POSITION[j]] = 10 - k
 
 # Print out the results table
 print("Resi Ty cons   int    vdw    ele    pol    npl   sasa  gas_e  rank grade max min")
-for j in range(min_resnum,max_resnum+1):
-        max = grade[j] + consurf[j] 
-        min = grade[j] - consurf[j] 
-        # We set ACE/NME max/min to 5 and consurf to 0 to ignore them
-        if resname[j] == "ACE" or resname[j] == "NME":
-            max = 5
-            min =5
-            consurf[j] = 0
-        print("{:>4}".format(resnumber[j]), "{:>3}".format(resname[j]), "  {:>1}".format(consurf[j]),\
-              "{:6.1f}".format(int_stab[j]), "{:6.1f}".format(vdw_stab[j]), "{:6.1f}".format(ele_stab[j]), "{:6.1f}".format(pol_stab[j]),\
-              "{:6.1f}".format(npl_stab[j]), "{:6.1f}".format(relsasa[j]),"{:6.1f}".format(gas_ene[j]), "  {:>2}".format(rank[j]),\
-              "   {:>2}".format(grade[j]), "{:>3}".format(max),  "{:>3}".format(min))
+for j in range(MIN_RESNUM, MAX_RESNUM+1):
+    maxi = GRADE[j] + CONSURF[j]
+    mini = GRADE[j] - CONSURF[j]
+    # We set ACE/NME max/min to 5 and CONSURF to 0 to ignore them
+    if RESNAME[j] == "ACE" or RESNAME[j] == "NME":
+        maxi = 5
+        mini = 5
+        CONSURF[j] = 0
+    print("{:>4}".format(RESNUMBER[j]), "{:>3}".format(RESNAME[j]), \
+          "  {:>1}".format(CONSURF[j]), "{:6.1f}".format(INT_STAB[j]), \
+          "{:6.1f}".format(VMD_STAB[j]), "{:6.1f}".format(ELE_STAB[j]), \
+          "{:6.1f}".format(POL_STAB[j]), "{:6.1f}".format(NPL_STAB[j]), \
+          "{:6.1f}".format(RELSASA[j]), "{:6.1f}".format(GAS_ENE[j]), \
+          "  {:>2}".format(RANK[j]), "   {:>2}".format(GRADE[j]), \
+          "{:>3}".format(max), "{:>3}".format(min))
