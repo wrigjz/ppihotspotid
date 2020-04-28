4a9ecdcaef6daee6a450757ed36f16b2506d1fbb
Jon Wright <jwright.taipei@gmail.com>
2020-04-28 18:29:59 +0800

    Added test for PBS/MPI

--- run_amber.sh
+++ run_amber.sh
@@ -36,12 +36,18 @@ $AMBERHOME/bin/tleap -f $scripts/leapin1
 # Run this to get the renumbering scheme we need later
 $AMBERHOME/bin/pdb4amber processed.pdb -o pre_mini.pdb  >| pre_mini.txt 2>&1
 
-export mpirun=/usr/local/openmpi-2.0.0_gcc48/bin/mpirun
-$mpirun -n `wc -l < $PBS_NODEFILE` -machinefile $PBS_NODEFILE \
+# Check if we are on the cluster
+if [ -z "$PBS_NODEFILE" ] ; then
+    echo "Non cluster run"
+    $AMBERHOME/bin/sander -O -i $scripts/sander.in -p pre_mini.top -c pre_mini.crd -ref pre_mini.crd \
+        -o post_mini.out -r post_mini.res
+else
+    echo "Cluster run"
+    export mpirun=/usr/local/openmpi-2.0.0_gcc48/bin/mpirun
+    $mpirun -n `wc -l < $PBS_NODEFILE` -machinefile $PBS_NODEFILE \
     $AMBERHOME/bin/sander.MPI -O -i $scripts/sander.in -p pre_mini.top -c pre_mini.crd -ref pre_mini.crd \
-    -o post_mini.out -r post_mini.res
-#$AMBERHOME/bin/sander -O -i $scripts/sander.in -p pre_mini.top -c pre_mini.crd -ref pre_mini.crd \
-#       -o post_mini.out -r post_mini.res
+        -o post_mini.out -r post_mini.res
+fi
 
 $AMBERHOME/bin/ambpdb -p pre_mini.top -c post_mini.res >| post_mini.pdb
 python $scripts/add_chain.py post_mini.pdb post_minix.pdb
