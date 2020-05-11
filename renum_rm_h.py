9b5dd7c42e44e32d8d14f1ecde8d78b414c29466
Jon Wright <jwright.taipei@gmail.com>
2020-05-11 10:54:46 +0800

    Allow removal of Hs without having to renumber

--- renum_rm_h.py
+++ renum_rm_h.py
@@ -9,12 +9,18 @@ import sys
 
 # A simple script to remove hydrogen atoms from a PDB file and renumber it from one
 # at the same time while writing out the renumerbing scheme
+# if you do not want to renumber then the 3rd argument must be No
 #
 # usage python3 remove_h.py input.pdb initial.pdb renumber.txt
+#       python3 remove_h.py input.pdb initial.pdb No
 
 INFILE = open(sys.argv[1], "r")
 OUTFILE = open(sys.argv[2], "w")
-RENFILE = open(sys.argv[3], "w")
+if sys.argv[3] == "No":
+    DORENUMBER = 0
+else:
+    DORENUMBER = 1
+    RENFILE = open(sys.argv[3], "w")
 
 RESID = 0
 PREVIOUS = -999
@@ -27,13 +33,17 @@ for LINE in INFILE:
         temp2 = LINE[22:27] # Gets the residue number
         word = temp1.strip()
         pdbresin = temp2.strip()
-        if pdbresin != PREVIOUS: # increment the pdb number if a new reisdue
-            RESID += 1
-            PREVIOUS = pdbresin
-            RENUMBER="{:>4s}".format(resname) + "{:>5s}".format(pdbresin) + \
-                     "{:>4s}".format(resname) + "{:>5d}".format(RESID) + "\n"
-            RENFILE.write(RENUMBER)
-        if word[0:1] != "H":
-            RESOUT = "{:>4s}".format(str(RESID))
-            LINEOUT = LINE[0:22] + RESOUT + " " + LINE[27:80] + "\n"
-            OUTFILE.write(LINEOUT)
+        if DORENUMBER == 1: # In the case of renumbering
+            if pdbresin != PREVIOUS: # increment the pdb number for each new reisdue
+                RESID += 1
+                PREVIOUS = pdbresin
+                RENUMBER="{:>4s}".format(resname) + "{:>5s}".format(pdbresin) + \
+                         "{:>4s}".format(resname) + "{:>5d}".format(RESID) + "\n"
+                RENFILE.write(RENUMBER)
+        if word[0:1] != "H": # For non-H's only
+            if DORENUMBER == 1:
+                RESOUT = "{:>4s}".format(str(RESID))
+                LINEOUT = LINE[0:22] + RESOUT + " " + LINE[27:80] + "\n"
+                OUTFILE.write(LINEOUT)
+            else:
+                OUTFILE.write(LINE)
