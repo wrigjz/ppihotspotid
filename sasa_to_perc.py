92bdfb173ea35d912f3d071b28f69f98258731d8
Jon Wright <jwright.taipei@gmail.com>
2020-05-20 16:07:10 +0800

    More updates for AmberTools20

--- sasa_to_perc.py
+++ sasa_to_perc.py
@@ -11,34 +11,33 @@
 
 import sys
 
-# This is the total SASA in the G-X-G tripeptide, we calc'd this ourselves
-SASAHAS = {
-    'ASP' : int(160.32),
-    'GLY' : int(87.75),
-    'ACE' : int(121.09),
-    'NME' : int(99.32),
-    'PRO' : int(142.52),
-    'THR' : int(149.97),
-    'VAL' : int(160.21),
-    'ALA' : int(116.69),
-    'CYS' : int(141.27),
-    'HIS' : int(192.75),
-    'LEU' : int(193.72),
-    'TRP' : int(257.93),
-    'ARG' : int(250.60),
-    'GLN' : int(193.09),
-    'ILE' : int(185.80),
-    'LYS' : int(214.48),
-    'SER' : int(126.58),
-    'TYR' : int(229.04),
-    'ASN' : int(163.73),
-    'GLU' : int(181.59),
-    'MET' : int(199.65),
-    'PHE' : int(214.27),
-    'HID' : int(193.24),
-    'HIE' : int(192.75),
-    'HIP' : int(192.60),
-    'CYX' : int(141.12),
+SASATAB = { # This is the total SASA in the G-X-G tripeptide, we calc'd this ourselves
+    'ASP' : '160.46',
+    'GLY' : '86.07',
+    'PRO' : '142.79',
+    'THR' : '152.87',
+    'VAL' : '162.36',
+    'ALA' : '115.28',
+    'CYS' : '145.10',
+    'HIS' : '198.65',
+    'LEU' : '193.09',
+    'TRP' : '263.02',
+    'ARG' : '254.28',
+    'GLN' : '196.21',
+    'ILE' : '188.05',
+    'LYS' : '219.01',
+    'SER' : '125.30',
+    'TYR' : '235.78',
+    'ASN' : '167.91',
+    'GLU' : '187.27',
+    'MET' : '201.98',
+    'PHE' : '221.01',
+    'HID' : '199.07',
+    'HIE' : '198.65',
+    'HIP' : '199.04',
+    'CYX' : '144.44',
+    'ACE' : '121.14',
+    'NME' : '99.49',
 }
 
 # Get the input name from the command line
@@ -57,6 +56,6 @@ for SASALINE in SASAFILE:
     SASASPLIT = SASALINE.split(":")   # separate at the :
     residue = SASALINE[12:15]
     temp = SASASPLIT[1].replace(" ", "")
-    sasadom = (SASAHAS.get(residue))
+    sasadom = (SASATAB.get(residue))
     relsasa = (float(temp) / sasadom)*100       # calculate the relative sasa
     print(SASALINE[0:10], SASALINE[11:24], " : {:7.2f}".format(relsasa))
