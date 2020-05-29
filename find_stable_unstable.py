09609270d4222306ce1724514827e770048e230f
Jon Wright <jwright.taipei@gmail.com>
2020-05-29 09:18:44 +0800

    Changed to appended arrays to save memory

--- find_stable_unstable.py
+++ find_stable_unstable.py
@@ -10,7 +10,7 @@
 # from column 14 and 15, we discard any that have a sasa < a cutoff
 #
 # We then look for any Stable residues vdw bonded to any Unstable residues (Matrix pair)
-# Then look for any other residues (SASA > cutoff) that are vdw bonded to 
+# Then look for any other residues (SASA > cutoff) that are vdw bonded to
 # either of the Stab/UnStab matrix pairs - we call these Bridge residues
 #
 # Stable are the low numbers e.g. energy rank - high consurf = v negative (min)
@@ -107,20 +107,15 @@ for i in range(0, MERGED_LIST_LEN):
 
 # Okay now we look for stable ----vdw --- unstable pairs
 # Start by reading in the NB2 file
-
 #X0002-PRO N   X0001-ACE CH3 2.46 MH  -2 -1.00  -1.0 -1.00  -1.0  27.8     1
-# Get an idea of the potential number of lines we will be dealing with
-INDEX_NB2 = -1
-NB2FILE = open("post_mini_noh.nb2", "r")
-for LINE in NB2FILE:
-    if LINE[0:1] == "X":
-        INDEX_NB2 += 1
-RESIDUE1 = [0 for i in range(0, INDEX_NB2+1)]
-RESIDUE2 = [0 for i in range(0, INDEX_NB2+1)]
-NETWORK = [0 for i in range(0, INDEX_NB2+1)]
-NB2FILE.seek(0)
+
+# Initial array setup
+RESIDUE1 = []
+RESIDUE2 = []
+NETWORK = []
 
 # Now read in the vdw pairs from the nb2 file and discard duplicates
+NB2FILE = open("post_mini_noh.nb2", "r")
 INDEX_NB2 = -1
 for LINE in NB2FILE:
     if LINE[0:1] == "X":
@@ -134,12 +129,13 @@ for LINE in NB2FILE:
         # This is a new pair so we will save it
         if marker == 0:
             INDEX_NB2 += 1
-            RESIDUE1[INDEX_NB2] = temp1
-            RESIDUE2[INDEX_NB2] = temp2
+            RESIDUE1.append(temp1)
+            RESIDUE2.append(temp2)
 
+#print(RESIDUE1)
 # Set up an array to save the pairs for the network residues
-SAVED_RESIDUE1 = [0 for i in range(0, INDEX_NB2+1)]
-SAVED_RESIDUE2 = [0 for i in range(0, INDEX_NB2+1)]
+SAVED_RESIDUE1 = []
+SAVED_RESIDUE2 = []
 #print(INDEX_ST, INDEX_UN, INDEX_NB2)
 
 # Now we do the actual search for the Stable/Unstable vdw bonded pairs (Matrix residues)
@@ -148,27 +144,29 @@ for k in range(0, INDEX_NB2+1):
     for i in range(0, INDEX_ST+1):
         for j in range(0, INDEX_UN+1):
             if (STABLE_NUMBER[i] == RESIDUE1[k] and UNSTABLE_NUMBER[j] == RESIDUE2[k]) or \
-               (STABLE_NUMBER[i] == RESIDUE2[k] and UNSTABLE_NUMBER[j] == RESIDUE1[k]):
-                   #print("Matrix Pair", STABLE_NAME[i], STABLE_NUMBER[i], UNSTABLE_NAME[j], \
-                   # UNSTABLE_NUMBER[j])
+                (STABLE_NUMBER[i] == RESIDUE2[k] and UNSTABLE_NUMBER[j] == RESIDUE1[k]):
+                #print("Matrix Pair", STABLE_NAME[i], STABLE_NUMBER[i], UNSTABLE_NAME[j], \
+                #    UNSTABLE_NUMBER[j])
                 MATRIX_NUMBER += 1
-                SAVED_RESIDUE1[MATRIX_NUMBER] = STABLE_NUMBER[i]
-                SAVED_RESIDUE2[MATRIX_NUMBER] = UNSTABLE_NUMBER[j]
+                SAVED_RESIDUE1.append(STABLE_NUMBER[i])
+                SAVED_RESIDUE2.append(UNSTABLE_NUMBER[j])
 
 # New we need to look for the network residues which are connected to either one of the
 # Stable / Unstable Matrix pairs
 #print("Network residues")
+#print(SAVED_RESIDUE1)
+#print(SAVED_RESIDUE2)
 INDEX_NET = -1
 for i in range(0, MATRIX_NUMBER+1):
     for j in range(0, INDEX_NB2+1):
         if SAVED_RESIDUE1[i] == RESIDUE1[j] or SAVED_RESIDUE2[i] == RESIDUE1[j]:
-            #print(RESIDUE2[j]) # Non-matching one
+            #print(RESIDUE2[j])
             INDEX_NET += 1
-            NETWORK[INDEX_NET] = int(RESIDUE2[j]) # Save the non-matching residue
+            NETWORK.append(int(RESIDUE2[j])) # Save the non-matching residue
         if SAVED_RESIDUE1[i] == RESIDUE2[j] or SAVED_RESIDUE2[i] == RESIDUE2[j]:
-            #print(RESIDUE1[j]) # Non-matching one
+            #print(RESIDUE1[j])
             INDEX_NET += 1
-            NETWORK[INDEX_NET] = int(RESIDUE1[j]) # Save the non-matching residue
+            NETWORK.append(int(RESIDUE1[j])) # Save the non-matching residue
 
 # Now try to remove network residues that are duplicates of the stable residues
 for i in range(0, INDEX_NET+1):
